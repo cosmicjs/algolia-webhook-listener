324ad84ab8ec8ee1149e32f35bf8c51900a61605
Chris Overstreet <chriso@chriso.io>
2018-06-30 21:33:22 -0400

    add edit endpoint

--- server.js
+++ server.js
@@ -67,6 +67,37 @@ server.post('/api/create', async (req, res) => {
   }
 });
 
+server.post('/api/edit', async (req, res) => {
+  try {
+    const { data } = req.body;
+    const { bucket, type_slug } = data;
+
+    const algoliaObject = convertCosmicObjToAlgoliaObj(data);
+    const bucketSlugRes = await searchBucket.getObject({ slug: bucket });
+    const projectBucketSlug = bucketSlugRes.object.content;
+
+    // Fetch algolia application id & admin api key
+    const projectBucket = Cosmic.bucket({ slug: projectBucketSlug });
+    const getKeysRes = await Promise.all([
+      projectBucket.getObject({ slug: 'algolia-info-application-id' }).catch(() => undefined),
+      projectBucket.getObject({ slug: 'algolia-info-admin-api-key' }).catch(() => undefined),
+    ]);
+
+    const applicationId = getKeysRes[0] && getKeysRes[0].object && getKeysRes[0].object.content;
+    const adminApiKey = getKeysRes[1] && getKeysRes[1].object && getKeysRes[1].object.content;
+
+    const client = algoliasearch(applicationId, adminApiKey);
+    const index = client.initIndex(type_slug);
+    const addRes = await index.addObject(algoliaObject);
+    const { taskID } = addRes;
+    await index.waitTask(taskID);
+    res.status(200).send();
+  } catch (e) {
+    console.error(e); // eslint-disable-line no-console
+    res.status(200).send();
+  }
+});
+
 server.listen(port, (err) => {
   if (err) throw err;
   // eslint-disable-next-line no-console
