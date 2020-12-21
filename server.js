0a5d9f30cdecd7be62ced82250c76d3624d0283f
Tony Spiro <tspiro@tonyspiro.com>
2020-12-21 10:12:11 -0600

    delete algolia object

--- server.js
+++ server.js
@@ -89,7 +89,6 @@ server.post('/api/edit', async (req, res) => {
     const { data } = req.body;
     const { read_key } = req.query;
     const { bucket, type_slug } = data;
-    const { read_key } = req.query;
 
     const algoliaObject = convertCosmicObjToAlgoliaObj(data);
     const searchBucket = Cosmic.bucket({ slug: 'algolia-search' });
@@ -120,40 +119,40 @@ server.post('/api/edit', async (req, res) => {
   }
 });
 
-// server.post('/api/delete', async (req, res) => {
-//   try {
-//     const { data } = req.body;
-//
-//     const {
-//       _id,
-//       bucket,
-//       type_slug,
-//     } = data;
-//
-//     const bucketSlugRes = await searchBucket.getObject({ slug: bucket });
-//     const projectBucketSlug = bucketSlugRes.object.content;
-//
-//     // Fetch algolia application id & admin api key
-//     const projectBucket = Cosmic.bucket({ slug: projectBucketSlug });
-//     const getKeysRes = await Promise.all([
-//       projectBucket.getObject({ slug: 'algolia-info-application-id' }).catch(() => undefined),
-//       projectBucket.getObject({ slug: 'algolia-info-admin-api-key' }).catch(() => undefined),
-//     ]);
-//
-//     const applicationId = getKeysRes[0] && getKeysRes[0].object && getKeysRes[0].object.content;
-//     const adminApiKey = getKeysRes[1] && getKeysRes[1].object && getKeysRes[1].object.content;
-//
-//     const client = algoliasearch(applicationId, adminApiKey);
-//     const index = client.initIndex(type_slug);
-//     const addRes = await index.deleteObject(_id);
-//     const { taskID } = addRes;
-//     await index.waitTask(taskID);
-//     res.status(200).send();
-//   } catch (e) {
-//     console.error(e); // eslint-disable-line no-console
-//     res.status(200).send();
-//   }
-// });
+server.post('/api/delete', async (req, res) => {
+  try {
+    const { data } = req.body;
+
+    const {
+      _id,
+      bucket,
+      type_slug,
+    } = data;
+
+    const bucketSlugRes = await searchBucket.getObject({ slug: bucket });
+    const projectBucketSlug = bucketSlugRes.object.content;
+
+    // Fetch algolia application id & admin api key
+    const projectBucket = Cosmic.bucket({ slug: projectBucketSlug });
+    const getKeysRes = await Promise.all([
+      projectBucket.getObject({ slug: 'algolia-info-application-id' }).catch(() => undefined),
+      projectBucket.getObject({ slug: 'algolia-info-admin-api-key' }).catch(() => undefined),
+    ]);
+
+    const applicationId = getKeysRes[0] && getKeysRes[0].object && getKeysRes[0].object.content;
+    const adminApiKey = getKeysRes[1] && getKeysRes[1].object && getKeysRes[1].object.content;
+
+    const client = algoliasearch(applicationId, adminApiKey);
+    const index = client.initIndex(type_slug);
+    const addRes = await index.deleteObject(_id);
+    const { taskID } = addRes;
+    await index.waitTask(taskID);
+    res.status(200).send();
+  } catch (e) {
+    console.error(e); // eslint-disable-line no-console
+    res.status(200).send();
+  }
+});
 
 server.listen(port, (err) => {
   if (err) throw err;
